5cd07aafa2d28dc8dd67ee8ee38640fc83b41035
Ivan Missana <ivanmissana@gmail.com>
2023-07-27 11:51:34 +0300

    another one

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -23,7 +23,7 @@ const form = useForm({
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                 ></textarea>
                 <InputError :message="form.errors.message" class="mt-2" />
-                <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+                <PrimaryButton class="mt-4 !bg-blue-500 hover:!bg-blue-700">Chirp</PrimaryButton>
             </form>
 
             <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
