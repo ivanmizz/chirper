54e6e2c91ebc17a4e7a82d19d995c3cf30282b3c
Ivan Missana <ivanmissana@gmail.com>
2023-07-27 17:14:18 +0300

    another one

--- resources/js/Components/Chirp.vue
+++ resources/js/Components/Chirp.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import dayjs from 'dayjs';
@@ -25,7 +26,7 @@ const editing = ref(false);
  
 <template>
     <div class="p-6 flex space-x-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100 " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
         </svg>
         <div class="flex-1">
@@ -39,18 +40,22 @@ const editing = ref(false);
                 <Dropdown v-if="chirp.user.id === $page.props.auth.user.id">
                     <template #trigger>
                         <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 hover:fill-black" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                             </svg>
                         </button>
 
                     </template>
                     <template #content>
-                        <button class="block w-full py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing=true" >
+                        <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing=true" >
                             Edit
                         </button>
-                    </template>
+                        <DropdownLink as="button" :href="route('chirps.destroy', chirp.id)" method="delete">
+                        Delete
+                        </DropdownLink>
 
+                    </template>
+                    
                 </Dropdown>
 
             </div>
